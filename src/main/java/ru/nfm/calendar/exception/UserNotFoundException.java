122ea516dcfabb6aa82ebf4a8f7b2c015e696b5d
NFM <daniel.olovianniy@gmail.com>
2023-09-27 05:55:53 +0300

    added exceptions

--- src/main/java/ru/nfm/calendar/exception/UserNotFoundException.java
+++ src/main/java/ru/nfm/calendar/exception/UserNotFoundException.java
@@ -1,11 +1,10 @@
 package ru.nfm.calendar.exception;
 
-import java.io.Serial;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException{
-
-    @Serial
-    private static final long serialVersionUID = 112312313L;
 
     public UserNotFoundException(long userId, String message) {
         super(String.format("Failed for [%o]: %s", userId, message));
