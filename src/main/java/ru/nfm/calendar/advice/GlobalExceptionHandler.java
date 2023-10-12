4439722496683a3e64b1df2f0665237d5c7dd0a0
NFM <daniel.olovianniy@gmail.com>
2023-10-13 02:37:25 +0300

    reduced code duplication

--- src/main/java/ru/nfm/calendar/advice/GlobalExceptionHandler.java
+++ src/main/java/ru/nfm/calendar/advice/GlobalExceptionHandler.java
@@ -2,7 +2,9 @@ package ru.nfm.calendar.advice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,8 +14,8 @@ import ru.nfm.calendar.exception.EmailAlreadyExistsException;
 import ru.nfm.calendar.exception.TokenRefreshException;
 
 import java.util.Date;
-import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
@@ -21,55 +23,38 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = TokenRefreshException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ErrorMessage handleTokenRefreshException(TokenRefreshException ex, WebRequest request) {
-        ErrorMessage errorMessage = new ErrorMessage(
-                HttpStatus.FORBIDDEN.value(),
-                new Date(),
-                ex.getMessage(),
-                request.getDescription(false)
-        );
-        log.error(errorMessage.toString());
-        return errorMessage;
+    public ResponseEntity<ErrorMessage> handleTokenRefreshException(TokenRefreshException ex, WebRequest request) {
+        return createErrorResponse(HttpStatus.FORBIDDEN, ex, request);
     }
 
     @ExceptionHandler(value = EmailAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorMessage handleEmailAlreadyExistsException(EmailAlreadyExistsException ex, WebRequest request) {
-        ErrorMessage errorMessage = new ErrorMessage(
-                HttpStatus.BAD_REQUEST.value(),
-                new Date(),
-                ex.getMessage(),
-                request.getDescription(false)
-        );
-        log.error(errorMessage.toString());
-        return errorMessage;
+    public ResponseEntity<ErrorMessage> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex, WebRequest request) {
+        return createErrorResponse(HttpStatus.BAD_REQUEST, ex, request);
     }
 
     @ExceptionHandler(value = AccessDeniedException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ErrorMessage handleAccessDeniedException(AccessDeniedException ex, WebRequest request) {
-        ErrorMessage errorMessage = new ErrorMessage(
-                HttpStatus.FORBIDDEN.value(),
-                new Date(),
-                ex.getMessage(),
-                request.getDescription(false)
-        );
-        log.error(errorMessage.toString());
-        return errorMessage;
+    public ResponseEntity<ErrorMessage> handleAccessDeniedException(AccessDeniedException ex, WebRequest request) {
+        return createErrorResponse(HttpStatus.FORBIDDEN, ex, request);
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorMessage handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
-        Map<String, String> errors = new LinkedHashMap<>();
-        ex.getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+    public ResponseEntity<ErrorMessage> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
+        Map<String, String> errors = ex.getFieldErrors().stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+        return createErrorResponse(HttpStatus.BAD_REQUEST, errors, request);
+    }
+
+    private ResponseEntity<ErrorMessage> createErrorResponse(HttpStatus status, Object error, WebRequest request) {
         ErrorMessage errorMessage = new ErrorMessage(
-                HttpStatus.BAD_REQUEST.value(),
+                status.value(),
                 new Date(),
-                errors,
+                error,
                 request.getDescription(false)
         );
         log.error(errorMessage.toString());
-        return errorMessage;
+        return ResponseEntity.status(status).body(errorMessage);
     }
 }
